38470147a41960b2693035a384203b47c41b33d8
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-21 03:14:00 +0000

    variables and data types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "designs.anup@gmail.com";
+var accountPassword = "12345";
+let accountCity = "Mumbai";
+let accountState;
+
+// accountId = 2;
+
+accountEmail = "change@gmail.com";
+accountPassword = "2121212";
+accountCity = "Pune";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treate all JS code as newer version
+
+// alert("hello world"); // we are using nodejs, not browser
+
+let name = "Anup"
+let age = 21
+let isLoggedIn = false
+
+console.table([typeof name, typeof age, typeof isLoggedIn]);

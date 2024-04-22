9b4ec161a81780f248c05451dd342714a23ad345
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-22 18:51:45 +0000

    updating conversion content

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,24 @@
-let score = 45
+let score = "45"
 
 console.log(typeof score);
 console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "45" => 45
+// "45adfr" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)

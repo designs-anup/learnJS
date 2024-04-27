5ac5139539685bd17ac367f6a1cdb69e484642b4
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-27 19:45:01 +0000

    objects

---
+++ 02_basic/02_objects.js
@@ -0,0 +1,20 @@
+// singleton
+
+// object.create
+
+// object litrals
+
+const jsUser = {
+    name : "Hitesh",
+    "full name" : "Hitesh Choudhary",
+    age : 18,
+    location : "Mumbai",
+    email : "hitesh@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+
+console.log(jsUser["full name"])

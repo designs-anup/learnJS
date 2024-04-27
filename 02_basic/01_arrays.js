7cb8b9479f95dba81b49cfa9b3f7dcf875647718
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-27 15:03:09 +0000

    updating array methods

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -11,5 +11,27 @@ const myArr2 = new Array(1, 2, 3, 4, 5)
 
 myArr.push(6)
 console.log(myArr);
-myArr.pop() // removes last value from the Array
+myArr.pop() // removes last value from the Array 
 console.log(myArr);
+
+myArr.unshift(1000) // add value to the start of the array [1000, 1, 2, 3, 4, 5]
+console.log(myArr);
+
+myArr.shift() // removed 1st element of the array
+console.log(myArr);
+
+console.log(myArr.includes(9)); // returns true/false
+console.log(myArr.indexOf(19)); // returns index value if present else -1ve value
+
+const newArr = myArr.join() // converted to string
+console.log(newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3) // [1, 2]
+console.log(myn1);
+
+const myn2 = myArr.splice(1, 3) // [1, 2, 3] also modify the original array
+console.log(myn2);

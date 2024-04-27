8e7f1c0add798931f09bfd874d9d639188d78de3
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-27 03:32:34 +0000

    date in Js

---
+++ 01_basic/07_datesinJs.js
@@ -0,0 +1,15 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("01-14-2023")   // MM DD YYYY
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());

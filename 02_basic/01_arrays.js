f6069afb2efc4b56e18c82887905a29ad550da54
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-27 19:24:34 +0000

    from array

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -35,3 +35,19 @@ console.log(myn1);
 
 const myn2 = myArr.splice(1, 3) // [1, 2, 3] also modify the original array
 console.log(myn2);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+//console.log(marvel_heros[3]); // [ 'superman', 'flash', 'batman' ]
+
+// console.log(marvel_heros.concat(dc_heros)); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) // convert to Array [ 'H', 'i', 't', 'e', 's', 'h' ]

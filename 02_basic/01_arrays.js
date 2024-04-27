66ae4fad3f42f0d8e22059fd8dc8e9fd1db3b5a8
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-27 03:47:53 +0000

    learn basic 02 array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,15 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[1]);
+
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr);
+myArr.pop() // removes last value from the Array
+console.log(myArr);

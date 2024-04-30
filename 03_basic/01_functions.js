0b47cde45ad8fdcbe93fa115cbf29190fffd8f20
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-04-30 18:34:36 +0000

    function rest parameter object and array

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -35,5 +35,34 @@ function loginUserMessage(username = "Jhon"){
 }
 
 // console.log( loginUserMessage("Hitesh") ); 
-console.log( loginUserMessage() );
+// console.log( loginUserMessage() );
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username : "Hitesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 1000, 500]));

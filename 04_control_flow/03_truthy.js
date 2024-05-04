b8a8b6b6199480dcfe561e42bc0b51ca52b5a626
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-05-04 20:21:20 +0000

    nullish and terniary operator

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -19,3 +19,19 @@ const emptyObject = {}
 if(Object.keys(emptyObject).length === 0){
     console.log("Empty Object");
 }
+
+// Nullish Coalescing Operater (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10;
+//val1 = null ?? 10;
+val1 = undefined ?? 15;
+
+console.log(val1);
+
+// Terniary Operator ( condition ? true : false )
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less tahn 80") : console.log("more than 80");
+

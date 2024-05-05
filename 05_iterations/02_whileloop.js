c2fe6bb756801f423c21d1e2cbef4a6e0cd4a841
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-05-05 13:53:34 +0000

    while do while

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,27 @@
+// while loop
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value of index is : ${index}`);
+
+    // index++
+    index = index + 2
+}
+
+let myArray = ["superman", "spiderman", "heman", "batman", "saktiman", "flash", "hulk", "ironman"]
+
+let arr = 0;
+while( arr < myArray.length ){
+    console.log(myArray[arr]);
+    arr++
+}
+
+// do while loop
+
+let score = 1
+    
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);

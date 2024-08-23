1078ec23783b0d9e06d840dde7b712b0f4b13d06
Anup Kumar Sahoo <designs.anup@gmail.com>
2024-08-23 23:44:10 +0530

    Update 08_reduce.js

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -8,3 +8,24 @@ const myTotal = myNums.reduce((acc, curval)=>{
 }, 0)
 
 console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 1999
+    },
+    {
+        itemName : "java course",
+        price : 2999
+    },
+    {
+        itemName : "swift course",
+        price : 3999
+    }
+]
+
+const shoppingCartTotal = shoppingCart.reduce( (acc, cartval) => {
+    return acc + cartval.price
+},0 )
+
+console.log(shoppingCartTotal);
